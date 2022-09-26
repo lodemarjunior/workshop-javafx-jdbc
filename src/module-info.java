49e505aa991190df02fe740023e32b0c689edd96
Lodemar Sansão Júnior <lodemarjunior@hotmail.com>
2022-09-26 19:48:23 -0300

    Adding database access

--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,7 @@ module workshop_javafx_jdbc {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires javafx.graphics;
+	requires java.sql;
 	
 	opens application to javafx.graphics, javafx.fxml;
 	
